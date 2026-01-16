ce9c4de093c8d6ef3d518df6786264485f328dd0
Ayushi Bhutani <ayushibhutani15@gmail.com>
2026-01-16 14:06:17 +0530

    tech stack ques

---
+++ Practice/PracticeSet1(CoreC#&DataTypes)/basic/Program.cs
@@ -0,0 +1,20 @@
+﻿// public class Program {
+
+//     public static void Main(String[] args){
+//         public string Number{get; set;}
+//         Console.WriteLine("Enter number");
+       
+
+
+//         try{
+//             int.TryParse(Console.ReadLine(), out int num);
+//             Console.WriteLine($"square of number is {num*num}");
+//         }catch(InvalidConversion ex){
+//             Console.WriteLine(ex.Message);
+//         }
+
+
+
+
+//     }
+// }
