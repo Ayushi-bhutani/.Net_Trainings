5b72dc46434b7d8976fe1f74117e64f634383107
Ayushi-bhutani <119058732+Ayushi-bhutani@users.noreply.github.com>
2026-02-02 15:09:49 +0530

    Implement unit tests for bank account operations
    Added unit tests for deposit and withdraw methods.

--- Practice/ScenarioBased/NUnit_bank/Program.cs
+++ Practice/ScenarioBased/NUnit_bank/Program.cs
@@ -1,16 +1,21 @@
-﻿using System;
+using System;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
 
 namespace bank
 {
+    // Bank account class (library code)
     public class Program
     {
         public decimal Balance { get; private set; }
-        //constructor for program class 
+
+        // Constructor
         public Program(decimal balance)
         {
             Balance = balance;
         }
-        //method to deposit amount
+
+        // Deposit method
         public void Deposit(decimal amount)
         {
             if (amount < 0)
@@ -18,13 +23,57 @@ namespace bank
 
             Balance += amount;
         }
-        //method to withdraw ammount
+
+        // Withdraw method
         public void Withdraw(decimal amount)
         {
             if (amount > Balance)
                 throw new Exception("Insufficient funds.");
 
             Balance -= amount;
+        }
+    }
+
+    // Unit test class
+    [TestFixture]
+    public class UnitTest
+    {
+        [Test]
+        public void Test_Deposit_ValidAmount()
+        {
+            Program account = new Program(100);
+            account.Deposit(50);
+
+            Assert.AreEqual(150, account.Balance);
+        }
+
+        [Test]
+        public void Test_Deposit_NegativeAmount()
+        {
+            Program account = new Program(100);
+
+            var ex = Assert.Throws<Exception>(() => account.Deposit(-20));
+
+            Assert.AreEqual("Deposit amount cannot be negative", ex.Message);
+        }
+
+        [Test]
+        public void Test_Withdraw_ValidAmount()
+        {
+            Program account = new Program(200);
+            account.Withdraw(50);
+
+            Assert.AreEqual(150, account.Balance);
+        }
+
+        [Test]
+        public void Test_Withdraw_InsufficientFunds()
+        {
+            Program account = new Program(100);
+
+            var ex = Assert.Throws<Exception>(() => account.Withdraw(150));
+
+            Assert.AreEqual("Insufficient funds.", ex.Message);
         }
     }
 }
